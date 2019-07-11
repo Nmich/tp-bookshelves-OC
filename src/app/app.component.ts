5659aad0ade8bef2f1179dca21e9ac1659fb97e5
Nmich <nmich.cor@gmail.com>
2019-07-11 16:32:33 +0200

    connexion firebase + authentification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'bookshelves';
+  constructor(){
+    let firebaseConfig = {
+      apiKey: "AIzaSyBXNW_48f8LX4PyzL2gM78I18JceHMg6C4",
+      authDomain: "bookshelves-ba6c0.firebaseapp.com",
+      databaseURL: "https://bookshelves-ba6c0.firebaseio.com",
+      projectId: "bookshelves-ba6c0",
+      storageBucket: "",
+      messagingSenderId: "1013102003211",
+      appId: "1:1013102003211:web:b25507a6defd2f7e"
+    };
+    firebase.initializeApp(firebaseConfig);
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,52 @@
-import { Injectable } from '@angular/core';
+import {
+  Injectable
+} from '@angular/core';
+import * as firebase from 'firebase';
+import {
+  resolve
+} from 'url';
+import {
+  reject
+} from 'q';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor() {}
+
+  createNewUser(email: string, password: string) {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.auth().createUserWithEmailAndPassword(email, password).then(
+          () => {
+            resolve();
+          },
+          (error) => {
+            reject(error);
+          }
+        )
+      }
+    );
+  }
+
+  signInUser(email: string, password: string) {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.auth().signInWithEmailAndPassword(email, password).then(
+          () => {
+            resolve();
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+  signOut() {
+    firebase.auth().signOut();
+  }
 }
